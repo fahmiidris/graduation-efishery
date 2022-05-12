226986d6c408880f23c7319839aefde5a0cf9922
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-05-12 08:34:45 +0700

    feat(favicon): add favicons and default seo image

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import clsx from 'clsx';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
+import { Favicons } from '@/components/favicons';
+
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const initialProps = await NextDocument.getInitialProps(ctx);
@@ -14,7 +16,10 @@ export default class Document extends NextDocument {
         lang="en"
         className={clsx('scroll-smooth [--scroll-mt:9.875rem] lg:[--scroll-mt:6.3125rem]')}
       >
-        <Head></Head>
+        <Head>
+          <Favicons />
+          <script></script>
+        </Head>
         <body
           className={clsx('font-sans text-slate-500 antialiased', {
             'bg-white': true,
