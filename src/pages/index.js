18f88c3da094c66c34c8e756762c72500fde9c26
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-05-12 10:47:32 +0700

    feat: add title and description to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
 import * as React from 'react';
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <h1 className="text-3xl font-bold text-slate-700">My Graduation</h1>
+          <p className="text-sm font-semibold">Fahmi Idris {'</>'} eFishery / eFishery Academy</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
