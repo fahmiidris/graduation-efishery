8b410349fcc6fa5a5fff16fe9eb2facc369190cd
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-08-05 19:51:30 +0700

    adjustment grow page

--- src/pages/grow.js
+++ src/pages/grow.js
@@ -1,21 +1,23 @@
-import { ArrowRightIcon } from '@heroicons/react/solid';
 import * as React from 'react';
+
+import { ArrowRightIcon } from '@heroicons/react/solid';
 
 const GrowPage = () => {
   return (
     <div className="container flex h-screen flex-col items-center justify-center">
-      <h1 className="text-xl font-semibold text-slate-700">Grow!!!</h1>
+      <h1 className="text-xl font-semibold text-slate-700">Perkembangan</h1>
 
-      <div className="flex w-full items-center justify-around pt-8">
-        <div className="text-semibold flex w-1/3 items-center justify-center text-2xl">
-          Diam Tulah Toleh, Bergerak Salah Kabeh!
-        </div>
-        <div className="flex w-1/3 items-center justify-center">
-          <ArrowRightIcon className="h-5 w-5" />
-        </div>
-        <div className="text-semibold flex w-1/3 items-center justify-center text-2xl">
-          Berkurang!
-        </div>
+      <div className="flex items-center space-x-8 pt-8">
+        <p className="rounded-sm border border-slate-400/20 px-4 py-2">
+          Diam Tulah Toleh, Bergerak Salah Kabeh.
+        </p>
+        <ArrowRightIcon className="h-5 w-5" />
+        <p className="rounded-sm border border-slate-400/20 px-4 py-2">Berkurang</p>
+      </div>
+
+      <div className="pt-8 text-center">
+        Dengan ada nya pengalaman magang di eFishery, membuat saya berfikir dulu sebelum bertindak,
+        berkumonikasi dulu untuk memastikan tidak ada kesalahpahaman.
       </div>
     </div>
   );
